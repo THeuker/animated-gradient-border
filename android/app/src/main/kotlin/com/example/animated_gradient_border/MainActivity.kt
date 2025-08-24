3d489f2c90e73df701032a1db3e08ffd2b9c1428
THeuker <thomas.heuker@live.nl>
2025-08-24 18:38:56 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/animated_gradient_border/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animated_gradient_border
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/animated_gradient_border.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+class AnimatedGradientBorder extends StatefulWidget {
+  const AnimatedGradientBorder({super.key});
+
+  @override
+  State<AnimatedGradientBorder> createState() => _AnimatedGradientBorderState();
+}
+
+class _AnimatedGradientBorderState extends State<AnimatedGradientBorder> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  
+  late Animation<Alignment> _tlAlignAnim;
+  late Animation<Alignment> _brAlignAnim;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _tlAlignAnim = TweenSequence<Alignment>([
+      TweenSequenceItem<Alignment>(
+        tween: Tween<Alignment>(begin: Alignment.topLeft, end: Alignment.topRight),
+        weight: 1
+      ),
+      TweenSequenceItem<Alignment>(
+        tween: Tween<Alignment>(begin: Alignment.topRight, end: Alignment.bottomRight),
+        weight: 1
+      ),
+      TweenSequenceItem<Alignment>(
+        tween: Tween<Alignment>(begin: Alignment.bottomRight, end: Alignment.bottomLeft),
+        weight: 1
+      ),
+      TweenSequenceItem<Alignment>(
+        tween: Tween<Alignment>(begin: Alignment.bottomLeft, end: Alignment.topLeft),
+        weight: 1
+      ),
+    ]).animate(_controller);
+
+    _brAlignAnim = TweenSequence<Alignment>([
+      TweenSequenceItem<Alignment>(
+        tween: Tween<Alignment>(begin: Alignment.bottomRight, end: Alignment.bottomLeft),
+        weight: 1
+      ),
+      TweenSequenceItem<Alignment>(
+        tween: Tween<Alignment>(begin: Alignment.bottomLeft, end: Alignment.topLeft),
+        weight: 1
+      ),
+      TweenSequenceItem<Alignment>(
+        tween: Tween<Alignment>(begin: Alignment.topLeft, end: Alignment.topRight),
+        weight: 1
+      ),
+      TweenSequenceItem<Alignment>(
+        tween: Tween<Alignment>(begin: Alignment.topRight, end: Alignment.bottomRight),
+        weight: 1
+      ),
+    ]).animate(_controller);
+
+    _controller.repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: _CenterCutPath(radius: 20, thickness: 2),
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, _) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(20)
+              ),
+              gradient: LinearGradient(
+                begin: _tlAlignAnim.value,
+                end: _brAlignAnim.value,
+                colors: const [Colors.red, Colors.blue],
+              ),
+            ),
+          );
+        }
+      ),
+    );
+  }
+}
+
+class _CenterCutPath extends CustomClipper<Path> {
+  final double radius;
+  final double thickness;
+
+  _CenterCutPath({
+    this.radius = 0,
+    this.thickness = 1,
+  });
+
+  @override
+  Path getClip(Size size) {
+    final rect = Rect.fromLTRB(
+      -size.width, -size.width, size.width * 2, size.height * 2
+    );
+
+    final double width = size.width - thickness * 2;
+    final double height = size.height - thickness * 2;
+
+    final path = Path()
+      ..fillType = PathFillType.evenOdd
+      ..addRRect(
+        RRect.fromRectAndRadius(
+          Rect.fromLTWH(thickness, thickness, width, height),
+          Radius.circular(radius - thickness),
+        ),
+      )
+      ..addRect(rect);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant _CenterCutPath oldClipper) {
+    return oldClipper.radius != radius || oldClipper.thickness != thickness;
+  }
+}
